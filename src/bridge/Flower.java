93340b92ddd98c7a327c937747c3d52cdef0eca3
rong77190 <18826077190r@gmail.com>
2017-04-16 21:23:56 +0800

    桥接

---
+++ src/bridge/Flower.java
@@ -0,0 +1,5 @@
+package bridge;
+
+public class Flower extends GiftImpl {
+
+}
---
+++ src/bridge/WildGift.java
@@ -0,0 +1,9 @@
+package bridge;
+
+public class WildGift extends Gift {
+
+	public WildGift(Ring ring) {
+		this.impl = impl;
+	}
+	
+}
